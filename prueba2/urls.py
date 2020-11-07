08a8d3ba0a8477bee303d7b5987b66ace6e5bd5a
pato <patricio@MacBook-Pro-de-patricio.local>
2020-11-06 21:24:10 -0300

    se agrego visto index y barra de navegacion

--- prueba2/urls.py
+++ prueba2/urls.py
@@ -13,6 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+""" 
+    Dicceionario de rutas!
+"""
 from django.contrib import admin
 from django.urls import path
 from prueba2.views import Inicio
--- prueba2/views.py
+++ prueba2/views.py
@@ -1,6 +1,17 @@
 from django.http import HttpResponse 
-
+from django.template import Template, Context
 #ejemplo de vista 
+
 def Inicio(request):
-    text = "Hola mundo"
-    return HttpResponse(text)
+    #Abrimos el documento que contiene la plantilla
+    PlantillaInicio = open("prueba2/plantillas/index.html")
+    #cargamos el documento en una variable de tipo 'Template'
+    template = Template(PlantillaInicio.read())
+    #Cerramos el documento externo 
+    PlantillaInicio.close()
+    #crear un contexto
+    contexto = Context()
+    #Renderizar el documento
+    documento = template.render(contexto)
+    return HttpResponse(documento)
+
